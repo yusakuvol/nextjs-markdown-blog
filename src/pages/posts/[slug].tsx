34ad3694912809264aa5f7ef422bfe4e1aa58909
yusakuvol <yoursakuyu@gmail.com>
2023-02-19 22:07:50 +0900

    feat: add slug static path

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -1,5 +1,25 @@
+import fs from 'fs'
+
 const Post = () => {
   return <div>コンテンツ</div>
+}
+
+export async function getStaticProps() {
+  return { props: { post: '' } }
+}
+
+export async function getStaticPaths() {
+  const files = fs.readdirSync('posts')
+  const paths = files.map((fileName) => ({
+    params: {
+      slug: fileName.replace(/\.md$/, ''),
+    },
+  }))
+  console.log('paths:', paths)
+  return {
+    paths,
+    fallback: false,
+  }
 }
 
 export default Post
